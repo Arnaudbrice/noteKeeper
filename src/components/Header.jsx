161e33b2aea78b8fab814061c33dd3aa2afdf4d3
arnaudbrice <mephisteur@gmail.com>
2025-08-12 20:44:28 +0200

    feature:add search function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useRef, useState} from "react";
 import {Link} from "react-router";
 import NoteContext from "../context/NoteContext.jsx";
 
-
 const Header = () => {
-  const {notes, setNotes} = useContext(NoteContext);
+  const {notes, setNotes, filteredNotes, setFilteredNotes} =
+    useContext(NoteContext);
+  const inputRef = useRef(null);
+  const [title, setTitle] = useState("");
+
+  // handle keydown event for search field focus using cmd+k or ctrl+k keys
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if ((event.metaKey || event.ctrlKey) && event.key.toLowerCase() === "k") {
+        event.preventDefault();
+        inputRef.current.focus();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  const handleSearchInputChange = (event) => {
+    const title = event.target.value;
+
+    setTitle(title);
+
+    if (title.trim() !== "") {
+      const filteredNotesByTitle = notes.filter((note) =>
+        note.title.toLowerCase().includes(title.toLowerCase()),
+      );
+
+      setFilteredNotes(filteredNotesByTitle);
+    } else {
+      setFilteredNotes(notes);
+    }
+  };
 
   return (
     <div className="text-lg shadow-sm navbar  sm:text-xl bg-[#172A36] ">
@@ -39,15 +71,20 @@ const Header = () => {
               Home
             </Link>
 
+            {/*for the dropdown menu*/}
+
+
             {notes.map((note, index) => (
               <Link
-                className="text-lg text-[#2D4453]  hover:py-6 hover:bg-[#2D4453] btn btn-ghost hover:text-white"
+                className="text-lg text-[#2D4453]  hover:py-6 hover:bg-[#2D4453] btn btn-ghost hover:text-white border-1  border-y-[#2D4453]  "
                 key={index}
                 to={`/note/:${note.id}`}
               >
                 {note.title}
               </Link>
             ))}
+
+
           </ul>
         </div>
       </div>
@@ -55,11 +92,54 @@ const Header = () => {
       <div
         className="navbar-center  flex flex-col sm:flex-row justify-center sm:justify-between gap-4 space-y-4 sm:space-y-0   space-x-4 py-4 sm:space-x-0 mx-auto sm:w-[90%] divide divide-y-1 divide-white sm:divide-y-none">
         <Link to="/">NoteKeeper</Link>
-        <input
-          type="text"
-          placeholder="Search"
-          className="input input-bordered  w-auto rounded-full"
-        />
+        {/*<input*/}
+        {/*  onChange={handleSearchInputChange}*/}
+        {/*  type="text"*/}
+        {/*  name="title"*/}
+        {/*  value={title}*/}
+        {/*  placeholder="Search"*/}
+        {/*  className="input input-bordered  w-auto rounded-full"*/}
+        {/*/>*/}
+
+        <label className="input  input-bordered  w-auto rounded-full">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            ref={inputRef}
+            onChange={handleSearchInputChange}
+            name="title"
+            value={title}
+            type="search"
+            className="grow "
+            placeholder="Search"
+          />
+
+          {navigator.platform.includes("Mac") ? (
+            <>
+              <kbd className="kbd kbd-sm">⌘</kbd>
+              <kbd className="kbd kbd-sm">K</kbd>
+            </>
+          ) : (
+            <>
+              <kbd className="kbd kbd-sm">Ctrl</kbd>
+              <kbd className="kbd kbd-sm">K</kbd>
+            </>
+          )}
+        </label>
       </div>
       {/*<div className=" navbar-end ">*/}
       {/*  <ul className="justify-around gap-6 px-4 text-xl menu menu-horizontal">*/}
@@ -69,7 +149,6 @@ const Header = () => {
       {/*  </ul>*/}
       {/*</div>*/}
     </div>
-
-  )
-}
-export default Header
+  );
+};
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,29 @@
 import React from "react";
+
+import {toast} from "react-toastify";
 import Form from "../components/Form";
 import Note from "../components/Note";
 
 import useNotes from "../hooks/useNotes";
 
-import {toast} from "react-toastify"
-
 const Home = () => {
-  const {notes, setNotes} = useNotes();
+  const {notes, setNotes, filteredNotes, setFilteredNotes} = useNotes();
 
   const handleAddNote = (note) => {
-
     setNotes((prevNotes) => {
       return [...prevNotes, note];
     });
 
-    toast.success(<div>Successfully added a new Note</div>)
+    toast.success(<div>Successfully added a new Note</div>);
   };
   return (
     <>
       <Form onHandleAddNote={handleAddNote}/>
-      <div
-        className="grid   grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 my-16 ">
-        {notes.map((note, index) =>
+      <div className="grid   grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 my-16 ">
+        {filteredNotes.map((note, index) => (
           <Note key={index} note={note}/>
-        )}
+        ))}
       </div>
-
     </>
   );
 };
